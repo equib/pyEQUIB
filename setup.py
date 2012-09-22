40b6ccbf4837237c20784d100c8453f8efe8c636
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2012-09-22 14:14:45 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#
+# Setup script for pyequib
+#
+
+import os
+import codecs
+try:
+      from setuptools import setup
+except ImportError:
+      from distutils.core import setup
+
+import pyequib
+
+with codecs.open('README.rst', 'r', 'utf-8') as fd:
+    setup(name='pyequib',
+          version=pyequib.__version__,
+          description = 'pyEQUIB: Python package for equilibrium atomic populations and line emissivities',
+          long_description=fd.read(),
+          author='Ashkbiz Danehkar',
+          author_email='ashkbiz.danehkar@students.mq.edu.au',
+          keywords = ['pyEQUIB', 'EQUIB', 'equilibrium', 'emissivities', 'atomic', 'astronomy', 'physics', 'astrophysics'],
+          license='http://www.gnu.org/licenses/gpl.html',
+          platforms=['any'],
+          packages=['pyequib'],
+          package_data={'pyequib': ['*.txt', 'text/*.readme', 'atomic-data/*.dat']},
+          install_requires=['numpy','scipy'],
+         )
