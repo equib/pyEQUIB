94de6ae8e9eec25dac1c7165e8a60ac19ef6d9c0
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2012-09-22 17:51:15 -0400

    ext.py

---
+++ pyequib/ext.py
@@ -0,0 +1,15 @@
+"""
+This module contains functions for extinction corrections using different laws
+"""
+
+import numpy, os
+import array, math
+from scipy import interpolate
+
+"""class _Extinction(object):
+    
+    def __init__(self):
+        pass
+""" 
+
+
