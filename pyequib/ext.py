b4308c5e7e60340d8c59cebdf59cb50ad2e1b443
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2012-09-29 12:10:16 -0400

    GAL2 function added

--- pyequib/ext.py
+++ pyequib/ext.py
@@ -91,6 +91,62 @@ def redlaw_gal(wave, rv=None):
          extl=val
    return (extl / 3.63) - 1.0
 
+def redlaw_gal2(wave):
+   """
+   NAME:
+       redlaw_smc
+   PURPOSE:
+      reddening law function for Galactic Savage & Mathis 1979
+   
+   EXPLANATION:
+   
+   CALLING SEQUENCE:
+       fl = redlaw_gal2(wave)
+   
+   INPUTS:
+       wave[] -  wavelength of emission line, Angstroms
+   RETURN: extl[] -  extinction evaluation array
+   
+   REVISION HISTORY:
+       Based on Savage & Mathis 1979, ARA&A, vol. 17, 73-111
+       Originally from IRAF STSDAS SYNPHOT ebmvxfunc.x
+       Initial IRAF implementation, R. A. Shaw, 20/09/1994
+       Return A(lambda)/A(V) instead, R. A. Shaw, 04/03/95
+       Converted to Python code by A. Danehkar, 31/08/2012
+   """
+   # Tabulated inverse wavelengths in microns:
+   xtable = numpy.array([0.00, 0.29, 0.45, 0.80, 1.11, 1.43, 1.82, 2.27, 2.50, 2.91, 3.65, 4.00, 4.17, 4.35, 4.57, 4.76, 5.00, 5.26, 5.56, 5.88, 6.25, 6.71, 7.18, 8.00, 8.50, 9.00, 9.50, 10.00])
+   
+   #  Tabulated extinction function, A(lambda)/E(B-V):
+   etable = numpy.array([0.00, 0.16, 0.38, 0.87, 1.50, 2.32, 3.10, 4.10, 4.40, 4.90, 6.20, 7.29, 8.00, 8.87, 9.67, 9.33, 8.62, 8.00, 7.75, 7.87, 8.12, 8.15, 8.49, 9.65, 10.55, 11.55, 12.90, 14.40])
+   
+   if hasattr(wave, "__len__"):
+     npts = len(wave)
+     extl = numpy.zeros(npts)
+   else:
+     npts = 1
+     extl = numpy.int32(0)
+   for pix in range(0, (npts - 1)+(1)):
+      if hasattr(wave, "__len__"):
+         wavel=wave[pix]
+      else:
+         wavel=wave
+      if (wavel < 1000.0):
+         print "redlaw_smc: Invalid wavelength"
+      # Convert wavelength in angstroms to 1/microns
+      x = 10000.e+0 / wavel
+      x = min([x, 10.0])
+      
+      # Linearly interpolate extinction law in 1/lam
+      val = lin_interp(etable, xtable, x)
+      #deriv1 = spl_init(xtab, extab)
+      #val=spl_interp(xtab, extab, deriv1, x)
+      if hasattr(extl, "__len__"):
+         extl[pix] = val
+      else:
+         extl=val
+   return (extl / 3.63) - 1.0
+
 def redlaw_ccm(wave, rv=None):
    """
     NAME:
