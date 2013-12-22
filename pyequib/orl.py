54f2948d2aa7da40f1a42e5f481598d56a72c3a3
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2013-12-22 11:55:34 -0500

    gamma4861

--- pyequib/orl.py
+++ pyequib/orl.py
@@ -8,6 +8,33 @@ import numpy, os
 import array, math
 from scipy import interpolate
 
+def gamma4861(temp, dens):
+   """
+    NAME:
+        gamm4861
+    PURPOSE:
+        determine the value of Log10 (gamm(H Beta))
+        = Log10( 4*Pai*j(HBeta)/NpNe) at temperature Te and density Ne
+        Storey P. J., Hummer D. G., 1995, MNRAS, 272, 41
+    EXPLANATION:
+   
+    CALLING SEQUENCE:
+        gamm4861_theory = gamma4861(temp,dens)
+   
+    INPUTS:
+        temp -     electron temperature in K
+        dens -     electron density in cm-3
+    RETURN:  Log10 (gamm(H Beta))
+   
+    REVISION HISTORY:
+        from Table 4.4 in D. E. Osterbrock & 
+        G. J. Ferland, Astrophysics of Gaseius Nebulae
+        and Active Galactic Nuclei, 2nd Ed., 2006
+        Python code by A. Danehkar, 31/08/2012
+   """
+   hb_ems = math.log10(hb_emissivity(temp, dens))
+   return hb_ems
+
 def gamma4471(temp, dens):
    """
     NAME:
