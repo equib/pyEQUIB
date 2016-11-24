680805d341a52b9eb0bb093ed492d10e73ccc51f
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2016-11-24 15:23:38 -0500

    some updates

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ except ImportError:
 
 import pyequib
 
+
 with codecs.open('README.rst', 'r', 'utf-8') as fd:
     setup(name='pyequib',
           version=pyequib.__version__,
@@ -20,6 +21,8 @@ with codecs.open('README.rst', 'r', 'utf-8') as fd:
           long_description=fd.read(),
           author='Ashkbiz Danehkar',
           author_email='ashkbiz.danehkar@students.mq.edu.au',
+          url='http://physics.mq.edu.au/~ashkbiz/pyequib/',
+          download_url = 'https://github.com/equib/pyEQUIB',
           keywords = ['pyEQUIB', 'EQUIB', 'equilibrium', 'emissivities', 'atomic', 'astronomy', 'physics', 'astrophysics'],
           license='http://www.gnu.org/licenses/gpl.html',
           platforms=['any'],
@@ -27,3 +30,4 @@ with codecs.open('README.rst', 'r', 'utf-8') as fd:
           package_data={'pyequib': ['*.txt', 'text/*.readme', 'atomic-data/*.dat']},
           install_requires=['numpy','scipy'],
          )
+
