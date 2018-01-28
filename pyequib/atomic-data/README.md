08f1567e4a206afa9b0a7df36040d2a41ba799ea
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2018-01-28 15:29:58 -0500

    Update appveyor.yml

--- pyequib/atomic-data/README.md
+++ pyequib/atomic-data/README.md
@@ -1,4 +1,4 @@
 ## Atomic Data
 
-The atomic data are **Chianti52** from the [CHIANTI atomic database](http://www.chiantidatabase.org/) version 5.2, and have been used in MOCASSIN v2.0.
+The atomic data are from the [CHIANTI atomic database](http://www.chiantidatabase.org/) version 5.2, and have been used in [MOCASSIN v2.0](https://github.com/mocassin/MOCASSIN-2.0).
 
--- pyequibtests/cel_test.py
+++ pyequibtests/cel_test.py
@@ -3,22 +3,22 @@
 import pyequib
 
 ion='o_iii'
-tempi=10000.0
-densi=5000.0
+temperature=10000.0
+density=5000.0
 levels5007='3,4/'
 iobs5007=1200.0
-Abb5007=pyequib.cel.calc_abundance(temperature=tempi, density=densi, line_flux=iobs5007, ion=ion, atomic_levels=levels5007)
+Abb5007=pyequib.cel.calc_abundance(temperature=temperature, density=density, line_flux=iobs5007, ion=ion, atomic_levels=levels5007)
 
 ion='s_ii'
-levu='1,2,1,3/'
-levl='1,5/'
-dens = 2550.0
+upper_levels='1,2,1,3/'
+lower_levels='1,5/'
+density = 2550.0
 niiTratio=10.753
-temp=pyequib.cel.calc_temperature(line_flux_ratio=niiTratio, density=dens, ion=ion, upper_levels=levu, lower_levels=levl)
+temp=pyequib.cel.calc_temperature(line_flux_ratio=niiTratio, density=density, ion=ion, upper_levels=upper_levels, lower_levels=lower_levels)
 
 ion='s_ii'
-levu='1,2/'
-levl='1,3/'
-temp = 7000.0
+upper_levels='1,2/'
+lower_levels='1,3/'
+temperature = 7000.0
 siiNratio=1.506
-dens=pyequib.cel.calc_density(line_flux_ratio=siiNratio, temperature=temp, ion=ion, upper_levels=levu, lower_levels=levl)
+dens=pyequib.cel.calc_density(line_flux_ratio=siiNratio, temperature=temperature, ion=ion, upper_levels=upper_levels, lower_levels=lower_levels)
