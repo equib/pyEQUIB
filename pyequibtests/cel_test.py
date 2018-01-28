7196fd991ff313cf97a9424f4af6f45652737558
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2018-01-28 14:59:25 -0500

    Update cel_test.py

--- pyequibtests/cel_test.py
+++ pyequibtests/cel_test.py
@@ -7,21 +7,18 @@ tempi=10000.0
 densi=5000.0
 levels5007='3,4/'
 iobs5007=1200.0
-Abb5007=pyequib.cel.calc_abundance(ion, levels5007, tempi, densi, iobs5007)
+Abb5007=pyequib.cel.calc_abundance(temperature=tempi, density=densi, line_flux=iobs5007, ion=ion, atomic_levels=levels5007)
 
 ion='s_ii'
 levu='1,2,1,3/'
 levl='1,5/'
-diagtype='T'
 dens = 2550.0
 niiTratio=10.753
-temp=pyequib.cel.calc_temp_dens(ion, levu, levl, niiTratio, diagtype, dens)
+temp=pyequib.cel.calc_temperature(line_flux_ratio=niiTratio, density=dens, ion=ion, upper_levels=levu, lower_levels=levl)
 
 ion='s_ii'
 levu='1,2/'
 levl='1,3/'
-diagtype='D'
 temp = 7000.0
 siiNratio=1.506
-dens=pyequib.cel.calc_temp_dens(ion, levu, levl, siiNratio, diagtype, temp)
-
+dens=pyequib.cel.calc_density(line_flux_ratio=siiNratio, temperature=temp, ion=ion, upper_levels=levu, lower_levels=levl)
